e735a8300fc48de5c06a740a5b9d038ecad77047
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2022-12-28 20:36:20 +0100

    add switch and sorted out js code

--- Z2J JavaScript vol2/js/script.js
+++ Z2J JavaScript vol2/js/script.js
@@ -1,22 +1,26 @@
-let num1 = parseFloat(prompt('Podaj pierwszą liczbę'))
-let operator = prompt('Podaj operator arytmetyczny(+, -, *, / lub %')
-let num2 = parseFloat(prompt('Podaj drugą liczbę'))
+const num1 = parseFloat(prompt('Podaj pierwszą liczbę'))
+const operator = prompt('Podaj operator arytmetyczny(+, -, *, / lub %')
+const num2 = parseFloat(prompt('Podaj drugą liczbę'))
 let result = 0
 
 if (isNaN(num1) || isNaN(num2)) {
 	alert('Użyłeś złego znaku, odśwież stronę!')
 } else {
-	if (operator == '+') {
-		result = +num1 + +num2
-	} else if (operator == '-') {
-		result = num1 - -(-num2)
-	} else if (operator == '*') {
-		result = num1 * num2
-	} else if (operator == '/') {
-		result = num1 / num2
-	} else if (operator == '%') {
-		result = num1 % num2
+	switch (operator) {
+		case '+':
+			alert(num1 + num2)
+			break
+		case '-':
+			alert(num1 - num2)
+			break
+		case '*':
+			alert(num1 * num2)
+			break
+		case '/':
+			alert(num1 / num2)
+			break
+		case '%':
+			alert(num1 % num2)
+			break
 	}
-
-	alert(num1 + operator + num2 + ' = ' + result)
 }
