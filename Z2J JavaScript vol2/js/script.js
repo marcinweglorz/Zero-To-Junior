317197c084c897d438528d8df544c46e533757c2
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2022-12-28 11:22:56 +0100

    add alert about wrong input in js code

--- Z2J JavaScript vol2/js/script.js
+++ Z2J JavaScript vol2/js/script.js
@@ -1,18 +1,22 @@
-let num1 = prompt('Podaj pierwszą liczbę')
+let num1 = parseFloat(prompt('Podaj pierwszą liczbę'))
 let operator = prompt('Podaj operator arytmetyczny(+, -, *, / lub %')
-let num2 = prompt('Podaj drugą liczbę')
+let num2 = parseFloat(prompt('Podaj drugą liczbę'))
 let result = 0
 
-if (operator == '+') {
-	result = +num1 + +num2
-} else if (operator == '-') {
-	result = num1-- - num2
-} else if (operator == '*') {
-	result = num1 * num2
-} else if (operator == '/') {
-	result = num1 / num2
-} else if (operator == '%') {
-	result = num1 % num2
-}
+if (isNaN(num1) || isNaN(num2)) {
+	alert('Użyłeś złego znaku, odśwież stronę!')
+} else {
+	if (operator == '+') {
+		result = +num1 + +num2
+	} else if (operator == '-') {
+		result = num1 - -(-num2)
+	} else if (operator == '*') {
+		result = num1 * num2
+	} else if (operator == '/') {
+		result = num1 / num2
+	} else if (operator == '%') {
+		result = num1 % num2
+	}
 
-alert(num1 + operator + num2 + ' = ' + result)
+	alert(num1 + operator + num2 + ' = ' + result)
+}
