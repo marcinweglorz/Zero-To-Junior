d4931cf6c179978ddb4a0449023ad720f1912bc0
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2022-12-30 22:00:50 +0100

    added divide by zero alert and while loop

--- Z2J JavaScript vol2/js/script.js
+++ Z2J JavaScript vol2/js/script.js
@@ -1,10 +1,12 @@
 const num1 = parseFloat(prompt('Podaj pierwszą liczbę'))
-const operator = prompt('Podaj operator arytmetyczny(+, -, *, / lub %')
+const operator = prompt('Podaj operator arytmetyczny(+, -, *, / lub %)')
 const num2 = parseFloat(prompt('Podaj drugą liczbę'))
-let result = 0
+let z2j = true
 
 if (isNaN(num1) || isNaN(num2)) {
 	alert('Użyłeś złego znaku, odśwież stronę!')
+} else if (operator === '/' && num2 === 0) {
+	alert('Nie dzielimy przez zero!')
 } else {
 	switch (operator) {
 		case '+':
@@ -22,5 +24,11 @@ if (isNaN(num1) || isNaN(num2)) {
 		case '%':
 			alert(num1 % num2)
 			break
+	}
+	while (z2j) {
+		let op2 = prompt('Podaj operator arytmetyczny(+, -, *, / lub %). Zostaw puste aby zakończyć.')
+		if (!op2) break
+		let num3 = prompt('Podaj kolejną liczbę')
+		alert(`${num1} ${op2} ${num3}`)
 	}
 }
