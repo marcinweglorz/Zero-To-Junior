3791fe9684df3b63286fb3105df85552abe12eea
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2023-01-03 21:57:49 +0100

    fixed code and added loop

--- Z2J JavaScript vol2/js/script.js
+++ Z2J JavaScript vol2/js/script.js
@@ -1,34 +1,38 @@
-const num1 = parseFloat(prompt('Podaj pierwszą liczbę'))
-const operator = prompt('Podaj operator arytmetyczny(+, -, *, / lub %)')
-const num2 = parseFloat(prompt('Podaj drugą liczbę'))
+let num1 = parseFloat(+prompt('Podaj pierwszą liczbę: '))
 let z2j = true
 
-if (isNaN(num1) || isNaN(num2)) {
-	alert('Użyłeś złego znaku, odśwież stronę!')
-} else if (operator === '/' && num2 === 0) {
-	alert('Nie dzielimy przez zero!')
-} else {
-	switch (operator) {
+while (z2j) {
+	let op = prompt('Podaj operator arytmetyczny(+, -, *, / lub %)')
+	if (!(op === '+' || op === '-' || op === '/' || op === '*' || op === '%')) break
+
+	let num2 = parseFloat(prompt('Podaj drugą liczbę: '))
+	switch (op) {
 		case '+':
-			alert(num1 + num2)
+			if (isNaN(num1) || isNaN(num2)) {
+				alert('Użyłeś złego znaku, popraw się!')
+			} else num1 += num2
 			break
 		case '-':
-			alert(num1 - num2)
-			break
-		case '*':
-			alert(num1 * num2)
+			if (isNaN(num1) || isNaN(num2)) {
+				alert('Użyłeś złego znaku, popraw się!')
+			} else num1 -= num2
 			break
 		case '/':
-			alert(num1 / num2)
+			if (isNaN(num1) || isNaN(num2)) {
+				alert('Użyłeś złego znaku, popraw się!')
+			} else if (num2 === 0) alert('Nie dzielimy przez zero!')
+			else num1 /= num2
+			break
+		case '*':
+			if (isNaN(num1) || isNaN(num2)) {
+				alert('Użyłeś złego znaku, popraw się!')
+			} else num1 *= num2
 			break
 		case '%':
-			alert(num1 % num2)
+			if (isNaN(num1) || isNaN(num2)) {
+				alert('Użyłeś złego znaku, popraw się!')
+			} else num1 %= num2
 			break
 	}
-	while (z2j) {
-		let op2 = prompt('Podaj operator arytmetyczny(+, -, *, / lub %). Zostaw puste aby zakończyć.')
-		if (!op2) break
-		let num3 = prompt('Podaj kolejną liczbę')
-		alert(`${num1} ${op2} ${num3}`)
-	}
+	alert(num1)
 }
